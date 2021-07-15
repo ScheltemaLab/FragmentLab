cccfd6fd09e14395374763ce6ded6c6a3aca1158
richard <r.a.scheltema@uu.nl>
2021-07-15 11:43:01 +0200

    Open search for a peptide definition or a single protein

---
+++ FragmentLab/dialogs/DialogPeptideSearchControl.Designer.cs
@@ -0,0 +1,314 @@
+﻿namespace FragmentLab.dialogs
+{
+	partial class DialogPeptideSearch
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.tabControl1 = new System.Windows.Forms.TabControl();
+			this.tabPeptide = new System.Windows.Forms.TabPage();
+			this.tabProtein = new System.Windows.Forms.TabPage();
+			this.editorType = new System.Windows.Forms.ComboBox();
+			this.editorModification = new System.Windows.Forms.ComboBox();
+			this.btnProteinRmMod = new System.Windows.Forms.Button();
+			this.btnProteinAddMod = new System.Windows.Forms.Button();
+			this.lblProteinModifications = new System.Windows.Forms.Label();
+			this.lblProteinProtease = new System.Windows.Forms.Label();
+			this.cmbProteinProtease = new System.Windows.Forms.ComboBox();
+			this.listProteinModifications = new HeckLib.visualization.ui.ListViewEx();
+			this.colModification = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.colType = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.txtProteinSequence = new System.Windows.Forms.TextBox();
+			this.lblProteinSequence = new System.Windows.Forms.Label();
+			this.btnOk = new System.Windows.Forms.Button();
+			this.btnCancel = new System.Windows.Forms.Button();
+			this.lblMinScore = new System.Windows.Forms.Label();
+			this.numMinScore = new System.Windows.Forms.NumericUpDown();
+			this.panel1.SuspendLayout();
+			this.tabControl1.SuspendLayout();
+			this.tabPeptide.SuspendLayout();
+			this.tabProtein.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.numMinScore)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// panel1
+			// 
+			this.panel1.Controls.Add(this.numMinScore);
+			this.panel1.Controls.Add(this.lblMinScore);
+			this.panel1.Controls.Add(this.tabControl1);
+			this.panel1.Controls.Add(this.btnOk);
+			this.panel1.Controls.Add(this.btnCancel);
+			this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.panel1.Location = new System.Drawing.Point(0, 0);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(347, 276);
+			this.panel1.TabIndex = 0;
+			// 
+			// tabControl1
+			// 
+			this.tabControl1.Controls.Add(this.tabPeptide);
+			this.tabControl1.Controls.Add(this.tabProtein);
+			this.tabControl1.Location = new System.Drawing.Point(0, 0);
+			this.tabControl1.Name = "tabControl1";
+			this.tabControl1.SelectedIndex = 0;
+			this.tabControl1.Size = new System.Drawing.Size(347, 239);
+			this.tabControl1.TabIndex = 4;
+			// 
+			// tabPeptide
+			// 
+			this.tabPeptide.Controls.Add(this.ctrlPeptideEditor);
+			this.tabPeptide.Location = new System.Drawing.Point(4, 22);
+			this.tabPeptide.Name = "tabPeptide";
+			this.tabPeptide.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPeptide.Size = new System.Drawing.Size(339, 213);
+			this.tabPeptide.TabIndex = 0;
+			this.tabPeptide.Text = "Peptide";
+			this.tabPeptide.UseVisualStyleBackColor = true;
+			// 
+			// ctrlPeptideEditor
+			// 
+			this.ctrlPeptideEditor.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.ctrlPeptideEditor.Location = new System.Drawing.Point(3, 3);
+			this.ctrlPeptideEditor.Name = "ctrlPeptideEditor";
+			this.ctrlPeptideEditor.Size = new System.Drawing.Size(333, 207);
+			this.ctrlPeptideEditor.TabIndex = 1;
+			// 
+			// tabProtein
+			// 
+			this.tabProtein.Controls.Add(this.editorType);
+			this.tabProtein.Controls.Add(this.editorModification);
+			this.tabProtein.Controls.Add(this.btnProteinRmMod);
+			this.tabProtein.Controls.Add(this.btnProteinAddMod);
+			this.tabProtein.Controls.Add(this.lblProteinModifications);
+			this.tabProtein.Controls.Add(this.lblProteinProtease);
+			this.tabProtein.Controls.Add(this.cmbProteinProtease);
+			this.tabProtein.Controls.Add(this.listProteinModifications);
+			this.tabProtein.Controls.Add(this.txtProteinSequence);
+			this.tabProtein.Controls.Add(this.lblProteinSequence);
+			this.tabProtein.Location = new System.Drawing.Point(4, 22);
+			this.tabProtein.Name = "tabProtein";
+			this.tabProtein.Padding = new System.Windows.Forms.Padding(3);
+			this.tabProtein.Size = new System.Drawing.Size(339, 213);
+			this.tabProtein.TabIndex = 1;
+			this.tabProtein.Text = "Protein";
+			this.tabProtein.UseVisualStyleBackColor = true;
+			// 
+			// editorType
+			// 
+			this.editorType.FormattingEnabled = true;
+			this.editorType.Location = new System.Drawing.Point(97, 148);
+			this.editorType.Name = "editorType";
+			this.editorType.Size = new System.Drawing.Size(121, 21);
+			this.editorType.TabIndex = 9;
+			this.editorType.Visible = false;
+			// 
+			// editorModification
+			// 
+			this.editorModification.FormattingEnabled = true;
+			this.editorModification.Location = new System.Drawing.Point(97, 121);
+			this.editorModification.Name = "editorModification";
+			this.editorModification.Size = new System.Drawing.Size(121, 21);
+			this.editorModification.TabIndex = 8;
+			this.editorModification.Visible = false;
+			// 
+			// btnProteinRmMod
+			// 
+			this.btnProteinRmMod.Location = new System.Drawing.Point(305, 117);
+			this.btnProteinRmMod.Name = "btnProteinRmMod";
+			this.btnProteinRmMod.Size = new System.Drawing.Size(26, 26);
+			this.btnProteinRmMod.TabIndex = 7;
+			this.btnProteinRmMod.Text = "-";
+			this.btnProteinRmMod.UseVisualStyleBackColor = true;
+			this.btnProteinRmMod.Click += new System.EventHandler(this.btnProteinRmMod_Click);
+			// 
+			// btnProteinAddMod
+			// 
+			this.btnProteinAddMod.Location = new System.Drawing.Point(305, 85);
+			this.btnProteinAddMod.Name = "btnProteinAddMod";
+			this.btnProteinAddMod.Size = new System.Drawing.Size(26, 26);
+			this.btnProteinAddMod.TabIndex = 6;
+			this.btnProteinAddMod.Text = "+";
+			this.btnProteinAddMod.UseVisualStyleBackColor = true;
+			this.btnProteinAddMod.Click += new System.EventHandler(this.btnProteinAddMod_Click);
+			// 
+			// lblProteinModifications
+			// 
+			this.lblProteinModifications.AutoSize = true;
+			this.lblProteinModifications.Location = new System.Drawing.Point(8, 85);
+			this.lblProteinModifications.Name = "lblProteinModifications";
+			this.lblProteinModifications.Size = new System.Drawing.Size(69, 13);
+			this.lblProteinModifications.TabIndex = 5;
+			this.lblProteinModifications.Text = "Modifications";
+			// 
+			// lblProteinProtease
+			// 
+			this.lblProteinProtease.AutoSize = true;
+			this.lblProteinProtease.Location = new System.Drawing.Point(8, 51);
+			this.lblProteinProtease.Name = "lblProteinProtease";
+			this.lblProteinProtease.Size = new System.Drawing.Size(49, 13);
+			this.lblProteinProtease.TabIndex = 4;
+			this.lblProteinProtease.Text = "Protease";
+			// 
+			// cmbProteinProtease
+			// 
+			this.cmbProteinProtease.FormattingEnabled = true;
+			this.cmbProteinProtease.Location = new System.Drawing.Point(85, 46);
+			this.cmbProteinProtease.Name = "cmbProteinProtease";
+			this.cmbProteinProtease.Size = new System.Drawing.Size(246, 21);
+			this.cmbProteinProtease.TabIndex = 3;
+			// 
+			// listProteinModifications
+			// 
+			this.listProteinModifications.AllowColumnReorder = true;
+			this.listProteinModifications.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.colModification,
+            this.colType});
+			this.listProteinModifications.DoubleClickActivation = false;
+			this.listProteinModifications.FullRowSelect = true;
+			this.listProteinModifications.HideSelection = false;
+			this.listProteinModifications.Location = new System.Drawing.Point(85, 85);
+			this.listProteinModifications.Name = "listProteinModifications";
+			this.listProteinModifications.Size = new System.Drawing.Size(214, 122);
+			this.listProteinModifications.TabIndex = 2;
+			this.listProteinModifications.UseCompatibleStateImageBehavior = false;
+			this.listProteinModifications.View = System.Windows.Forms.View.Details;
+			// 
+			// colModification
+			// 
+			this.colModification.Text = "Modification";
+			this.colModification.Width = 132;
+			// 
+			// colType
+			// 
+			this.colType.Text = "Type";
+			this.colType.Width = 77;
+			// 
+			// txtProteinSequence
+			// 
+			this.txtProteinSequence.Location = new System.Drawing.Point(85, 19);
+			this.txtProteinSequence.Name = "txtProteinSequence";
+			this.txtProteinSequence.Size = new System.Drawing.Size(246, 20);
+			this.txtProteinSequence.TabIndex = 1;
+			// 
+			// lblProteinSequence
+			// 
+			this.lblProteinSequence.AutoSize = true;
+			this.lblProteinSequence.Location = new System.Drawing.Point(8, 22);
+			this.lblProteinSequence.Name = "lblProteinSequence";
+			this.lblProteinSequence.Size = new System.Drawing.Size(56, 13);
+			this.lblProteinSequence.TabIndex = 0;
+			this.lblProteinSequence.Text = "Sequence";
+			// 
+			// btnOk
+			// 
+			this.btnOk.Location = new System.Drawing.Point(188, 245);
+			this.btnOk.Name = "btnOk";
+			this.btnOk.Size = new System.Drawing.Size(75, 23);
+			this.btnOk.TabIndex = 2;
+			this.btnOk.Text = "&OK";
+			this.btnOk.UseVisualStyleBackColor = true;
+			this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+			// 
+			// btnCancel
+			// 
+			this.btnCancel.Location = new System.Drawing.Point(269, 245);
+			this.btnCancel.Name = "btnCancel";
+			this.btnCancel.Size = new System.Drawing.Size(75, 23);
+			this.btnCancel.TabIndex = 3;
+			this.btnCancel.Text = "&Cancel";
+			this.btnCancel.UseVisualStyleBackColor = true;
+			this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+			// 
+			// lblMinScore
+			// 
+			this.lblMinScore.AutoSize = true;
+			this.lblMinScore.Location = new System.Drawing.Point(4, 250);
+			this.lblMinScore.Name = "lblMinScore";
+			this.lblMinScore.Size = new System.Drawing.Size(53, 13);
+			this.lblMinScore.TabIndex = 5;
+			this.lblMinScore.Text = "Min score";
+			// 
+			// numMinScore
+			// 
+			this.numMinScore.DecimalPlaces = 1;
+			this.numMinScore.Location = new System.Drawing.Point(62, 247);
+			this.numMinScore.Name = "numMinScore";
+			this.numMinScore.Size = new System.Drawing.Size(64, 20);
+			this.numMinScore.TabIndex = 6;
+			this.numMinScore.Value = new decimal(new int[] {
+            25,
+            0,
+            0,
+            0});
+			// 
+			// DialogPeptideSearch
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(347, 276);
+			this.Controls.Add(this.panel1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "DialogPeptideSearch";
+			this.Text = "Open search";
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			this.tabControl1.ResumeLayout(false);
+			this.tabPeptide.ResumeLayout(false);
+			this.tabProtein.ResumeLayout(false);
+			this.tabProtein.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.numMinScore)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Button btnOk;
+		private System.Windows.Forms.Button btnCancel;
+		private HeckLib.graphics.controls.PeptideEditorControl ctrlPeptideEditor;
+		private System.Windows.Forms.TabControl tabControl1;
+		private System.Windows.Forms.TabPage tabPeptide;
+		private System.Windows.Forms.TabPage tabProtein;
+		private System.Windows.Forms.Label lblProteinProtease;
+		private System.Windows.Forms.ComboBox cmbProteinProtease;
+		private HeckLib.visualization.ui.ListViewEx listProteinModifications;
+		private System.Windows.Forms.TextBox txtProteinSequence;
+		private System.Windows.Forms.Label lblProteinSequence;
+		private System.Windows.Forms.Label lblProteinModifications;
+		private System.Windows.Forms.Button btnProteinRmMod;
+		private System.Windows.Forms.Button btnProteinAddMod;
+		private System.Windows.Forms.ColumnHeader colModification;
+		private System.Windows.Forms.ColumnHeader colType;
+		private System.Windows.Forms.ComboBox editorType;
+		private System.Windows.Forms.ComboBox editorModification;
+		private System.Windows.Forms.NumericUpDown numMinScore;
+		private System.Windows.Forms.Label lblMinScore;
+	}
+}
---
+++ FragmentLab/dialogs/DialogPeptideSearchControl.cs
@@ -0,0 +1,418 @@
+﻿/* Copyright (C) 2015, Biomolecular Mass Spectrometry and Proteomics (http://hecklab.com)
+ * This file is part of HeckLib.
+ *
+ * HeckLib is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * HeckLib is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with HeckLib; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+
+
+// C#
+using System;
+using System.IO;
+using System.Threading;
+
+using System.Collections;
+using System.Collections.Generic;
+
+using System.Windows;
+using System.Windows.Forms;
+
+// HeckLib
+using HeckLib;
+using HeckLib.utils;
+using HeckLib.masspec;
+using HeckLib.chemistry;
+using HeckLib.backgroundworker;
+
+using HeckLib.io;
+using HeckLib.io.database;
+using HeckLib.io.opensearch;
+
+
+
+
+
+namespace FragmentLab.dialogs
+{
+	public partial class DialogPeptideSearch : Form
+	{
+		#region constructor(s)
+		public DialogPeptideSearch(Document document, FragmentLabSettings settings, HeckLibSettingsDatabase hecklab_settings)
+		{
+			this.ctrlPeptideEditor = new HeckLib.graphics.controls.PeptideEditorControl(hecklab_settings.GetModifications()); // <- this was moved out of the InitializeComponent function
+
+			// windows related
+			this.StartPosition = FormStartPosition.CenterParent;
+			this.CancelButton = btnCancel;
+			InitializeComponent();
+
+			// events
+			listProteinModifications.DoubleClickActivation	= true;
+			listProteinModifications.SubItemClicked			+= ListProteinModifications_SubItemClicked;
+			listProteinModifications.SubItemEndEditing		+= ListProteinModifications_SubItemEndEditing;
+
+			// add the proteases
+			m_dProteases = new Dictionary<string, Protease>();
+			foreach (Protease protease in Protease.Proteases)
+			{
+				m_dProteases.Add(protease.Title, protease);
+				cmbProteinProtease.Items.Add(protease.Title);
+			}
+			cmbProteinProtease.SelectedItem = Protease.Proteases[Protease.TrypsinP].Title;
+
+			// fill the editors
+			m_dModifications = hecklab_settings.GetModifications();
+			foreach (Modification m in m_dModifications.Values)
+				editorModification.Items.Add(m.Title);
+
+			editorType.Items.Add("FIXED");
+			editorType.Items.Add("VARIABLE");
+
+			// general bookkeeping
+			m_pHecklibSettings = hecklab_settings;
+			m_pSettings = settings;
+			m_pDocument = document;
+		}
+		#endregion
+
+
+		#region access
+		public List<PeptideSpectrumMatch> Result { get { return m_lResult; } }
+		#endregion
+
+
+		#region events
+		private void btnOk_Click(object sender, EventArgs e)
+		{
+			PeptideSearchData data;
+			if (tabControl1.SelectedTab == tabPeptide)
+			{
+				// extract the information from the UI
+				Peptide peptide = ctrlPeptideEditor.SelectedPeptide;
+				if (peptide == null || peptide.Length == 0)
+				{
+					System.Windows.MessageBox.Show("No peptide defined, exiting.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+					DialogResult = DialogResult.Abort; Close();
+					return;
+				}
+
+				data = new PeptideSearchData {
+						MinScore				= (double)numMinScore.Value,
+						SelectedPeptide			= peptide,
+					};
+			}
+			else if (tabControl1.SelectedTab == tabProtein)
+			{
+				if (string.IsNullOrEmpty(txtProteinSequence.Text) || !m_dProteases.ContainsKey(cmbProteinProtease.Text))
+				{
+					System.Windows.MessageBox.Show("No protein information defined, exiting.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+					DialogResult = DialogResult.Abort; Close();
+					return;
+				}
+
+				string sequence = txtProteinSequence.Text;
+				Protease protease = m_dProteases[cmbProteinProtease.Text];
+				List<Modification> fixmods = new List<Modification>(), varmods = new List<Modification>();
+				foreach (ListViewItem item in listProteinModifications.Items)
+				{
+					string title = item.SubItems[0].Text;
+					string type = item.SubItems[1].Text;
+
+					if (!m_dModifications.ContainsKey(title))
+					{
+						System.Windows.MessageBox.Show("Selected modification '" + title + "' not known, exiting.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+						DialogResult = DialogResult.Abort; Close();
+						return;
+					}
+
+					if (type == "FIXED")
+						fixmods.Add(m_dModifications[title]);
+					else
+						varmods.Add(m_dModifications[title]);
+				}
+
+				data = new PeptideSearchData {
+						MinScore				= (double)numMinScore.Value,
+						ProteinSequence			= sequence,
+						Protease				= protease,
+						FixMods					= fixmods.ToArray(),
+						VarMods					= varmods.ToArray()
+					};
+			}
+			else
+				throw new Exception("Unexpected tab for open search ?");
+
+			// process the file
+			m_pBackgroundWorker.TriggerBackgroundWorker(Process, data);
+
+			// we're done
+			DialogResult = DialogResult.OK;
+			this.Close();
+		}
+
+		private void btnCancel_Click(object sender, EventArgs e)
+		{
+			DialogResult = DialogResult.Cancel;
+			this.Close();
+		}
+
+		private void ListProteinModifications_SubItemClicked(object sender, HeckLib.visualization.ui.SubItemEventArgs e)
+		{
+			Control ctrl = null;
+			switch (e.SubItem)
+			{
+				case 0:
+					ctrl = editorModification;
+					break;
+				case 1:
+					ctrl = editorType;
+					break;
+			};
+			if (ctrl == null)
+				return;
+
+			listProteinModifications.StartEditing(ctrl, e.Item, e.SubItem);
+		}
+
+		private void ListProteinModifications_SubItemEndEditing(object sender, HeckLib.visualization.ui.SubItemEndEditingEventArgs e)
+		{
+			if (e.Cancel)
+				return;
+
+			// grab the data from the editor
+			if (e.SubItem == 0)
+			{
+			}
+			else if (e.SubItem == 1)
+			{
+			}
+		}
+
+		private void btnProteinAddMod_Click(object sender, EventArgs e)
+		{
+			listProteinModifications.Items.Add(new ListViewItem(new string[] { editorModification.Items[0].ToString(), editorType.Items[0].ToString() }));
+		}
+
+		private void btnProteinRmMod_Click(object sender, EventArgs e)
+		{
+			if (listProteinModifications.SelectedIndices.Count == 0)
+				return;
+			int index = listProteinModifications.SelectedIndices[0];
+			if (index < 0 || index >= listProteinModifications.Items.Count)
+				return;
+			listProteinModifications.Items.RemoveAt(index);
+		}
+		#endregion
+
+
+		#region processor
+		private class PeptideSearchData
+		{
+			public double MinScore;
+
+			// protein search
+			public string ProteinSequence	= null;
+			public Modification[] VarMods	= null;
+			public Modification[] FixMods	= null;
+			public Protease Protease		= null;
+
+			// peptide search
+			public Peptide SelectedPeptide	= null;
+		}
+
+		private void CreatePeptideIndex(string dbpath, PeptideSearchData data, out OpenSearchDatabase targetdb, out FragmentIndex fragmentindex)
+		{
+			OpenSearch.DbSettings settings = new HeckLib.io.opensearch.OpenSearch.DbSettings {
+						Format					= HeckLib.io.fasta.FastaParser.Format.FASTA,
+						MinPeptideLength		= 5,
+						MaxPeptideLength		= 50,
+						Processing				= HeckLib.io.opensearch.OpenSearch.SequenceProcessing.TARGET,
+						MinMass					= 500,
+						MaxMass					= 10000,
+						MaxFragmentCharge		= 2,
+						MinFragmentMz			= 100,
+						MaxFragmentMz			= 1500,
+						MaxNrMods				= 3,
+						FixedMods				= data.SelectedPeptide.GetUniqueModifications(),
+						VarMods					= new HeckLib.chemistry.Modification[0],
+						ProteaseCascade			= new HeckLib.io.opensearch.OpenSearch.ProteaseSettings[0]
+					};
+			OpenSearch.GenerateDbFromPeptideDefinition(data.SelectedPeptide, Path.Combine(dbpath, "peptide.db"), settings, null);
+
+			targetdb = new OpenSearchDatabase(
+					Path.Combine(dbpath, "peptide.masses"),
+					Path.Combine(dbpath, "peptide.modpeps"),
+					Path.Combine(dbpath, "peptide.modificationdescriptions"),
+					Path.Combine(dbpath, "peptide.proteins")
+				);
+			fragmentindex = new FragmentIndex(Path.Combine(dbpath, "peptide.fragmentindex"));
+		}
+
+		private void CreateProteinIndex(string dbpath, PeptideSearchData data, out OpenSearchDatabase targetdb, out FragmentIndex fragmentindex)
+		{
+			OpenSearch.DbSettings settings = new HeckLib.io.opensearch.OpenSearch.DbSettings {
+						Format					= HeckLib.io.fasta.FastaParser.Format.FASTA,
+						MinPeptideLength		= 5,
+						MaxPeptideLength		= 50,
+						Processing				= HeckLib.io.opensearch.OpenSearch.SequenceProcessing.TARGET,
+						MinMass					= 500,
+						MaxMass					= 10000,
+						MaxFragmentCharge		= 2,
+						MinFragmentMz			= 100,
+						MaxFragmentMz			= 1500,
+						MaxNrMods				= 3,
+						FixedMods				= data.FixMods,
+						VarMods					= data.VarMods,
+						ProteaseCascade			= new OpenSearch.ProteaseSettings[]{
+								new OpenSearch.ProteaseSettings { Protease = data.Protease, MaxMissedCleavages = 3 }
+							}
+					};
+			OpenSearch.GenerateDbFromProteinSequence(data.ProteinSequence, Path.Combine(dbpath, "protein.db"), settings, null);
+
+			targetdb = new OpenSearchDatabase(
+					Path.Combine(dbpath, "protein.masses"),
+					Path.Combine(dbpath, "protein.modpeps"),
+					Path.Combine(dbpath, "protein.modificationdescriptions"),
+					Path.Combine(dbpath, "protein.proteins")
+				);
+			fragmentindex = new FragmentIndex(Path.Combine(dbpath, "protein.fragmentindex"));
+		}
+
+		private class PeptideMatchInfo
+		{
+			public uint PeptideId;
+			public int N_b = 0;
+			public int N_y = 0;
+			public int sumI_b = 0;
+			public int sumI_y = 0;
+		}
+
+		private void Process(PeptideSearchData data, IProgress<int> progress, CancellationToken iscancelled)
+		{
+			// generate the database
+			string dbpath = FilesA.CreateTemporaryDirectory();
+
+			OpenSearchDatabase targetdb; FragmentIndex fragmentindex;
+			if (data.SelectedPeptide != null)
+				CreatePeptideIndex(dbpath, data, out targetdb, out fragmentindex);
+			else if (data.ProteinSequence != null)
+				CreateProteinIndex(dbpath, data, out targetdb, out fragmentindex);
+			else
+				throw new Exception("Unexpected sequence of events for OpenSearch.");
+			
+			// process the spectra
+			List<PeptideSpectrumMatch> base_psms = m_pDocument.GetPsms();
+			List<PeptideSpectrumMatch> resulting_psms = new List<PeptideSpectrumMatch>();
+
+			int nr_psms = 0;
+			foreach (PeptideSpectrumMatch base_psm in base_psms)
+			{
+				progress.Report((int)(100.0 * nr_psms++ / base_psms.Count));
+
+				int[] topxranks;
+				PeptideFragment.FragmentModel model;
+				INoiseDistribution noise; PrecursorInfo precursor; ScanHeader scanheader;
+				Centroid[] spectrum = m_pDocument.LoadSpectrum(base_psm, m_pSettings, out topxranks, out model, out noise, out precursor, out scanheader);
+				if (spectrum.Length < 5)
+					continue;
+
+				// de-de-isotope
+				for (int i = 0; i < spectrum.Length; ++i)
+					if (spectrum[i].Charge != 0 && spectrum[i].Charge != 1) spectrum[i].Mz = MassSpectrometry.Recharge(spectrum[i].Mz, 1, spectrum[i].Charge);
+
+				double[] mzs; float[] intensities;
+				Centroid.GetMzsIntensities(spectrum, out mzs, out intensities);
+				float maxintensity = Statistics.Max(intensities);
+				for (int i = 0; i < intensities.Length; ++i)
+					intensities[i] = 1 + (float)Math.Round(99 * intensities[i] / maxintensity);
+
+				// match our peptide against the spectrum
+				Dictionary<uint, PeptideMatchInfo> matches = new Dictionary<uint, PeptideMatchInfo>();
+				for (int i = 0; i < spectrum.Length; ++i)
+				{
+					uint[] matching_peptideids; byte[] matched_fragmenttype;
+					fragmentindex.MatchFragment(m_pSettings.MatchTolerance, spectrum[i].Mz, spectrum[i].Charge, null, out matching_peptideids, out matched_fragmenttype);
+
+					for (int idx = 0; idx < matching_peptideids.Length; ++idx)
+					{
+						if (!matches.ContainsKey(matching_peptideids[idx]))
+							matches.Add(matching_peptideids[idx], new PeptideMatchInfo { PeptideId = matching_peptideids[idx] });
+						
+						if (matched_fragmenttype[idx] == PeptideFragment.ION_B)
+						{
+							matches[matching_peptideids[idx]].N_b++;
+							matches[matching_peptideids[idx]].sumI_b += (int)intensities[i];
+						}
+						else if (matched_fragmenttype[idx] == PeptideFragment.ION_Y)
+						{
+							matches[matching_peptideids[idx]].N_y++;
+							matches[matching_peptideids[idx]].sumI_y += (int)intensities[i];
+						}
+					}
+				}
+
+				List<PeptideSpectrumMatch> psms = new List<PeptideSpectrumMatch>();
+				foreach (PeptideMatchInfo match in matches.Values)
+				{
+					if (match.N_b < 2 || match.N_y < 2)
+						continue;
+					double score = SpectrumUtils.CalcHyperScore_by(match.N_b, match.N_y, match.sumI_b, match.sumI_y);
+					if (score < data.MinScore)
+						continue;
+
+					// store this as a psm
+					Peptide[] p = targetdb.GetModifiedPeptidesFromIds(new int[] { (int)match.PeptideId });
+
+					PeptideSpectrumMatch psm = new PeptideSpectrumMatch {
+							RawFile				= base_psm.RawFile,
+							Mz					= base_psm.Mz,
+							Charge				= base_psm.Charge,
+							MinScan				= base_psm.MinScan,
+							RetentionTime		= base_psm.RetentionTime,
+							Peptide				= p[0],
+							Score				= score,
+							Fragmentation		= precursor.Fragmentation
+						};
+					psms.Add(psm);
+				}
+				if (psms.Count > 0)
+				{
+					psms.Sort((a, b) => -a.Score.CompareTo(b.Score));
+					resulting_psms.Add(psms[0]);
+				}
+			}
+			m_lResult = resulting_psms;
+
+			// destroy the temporary database
+			targetdb.Dispose();
+			FilesA.DeleteDirectory(dbpath);
+		}
+		#endregion
+
+
+		#region data
+		private Document m_pDocument;
+		private FragmentLabSettings m_pSettings;
+		private HeckLibSettingsDatabase m_pHecklibSettings;
+
+		private Dictionary<string, Protease> m_dProteases;
+		private Dictionary<string, Modification> m_dModifications;
+
+		private HlBackgroundWorker<PeptideSearchData> m_pBackgroundWorker = new HlBackgroundWorker<PeptideSearchData>();
+
+		private List<PeptideSpectrumMatch> m_lResult = new List<PeptideSpectrumMatch>();
+		#endregion
+	}
+}
